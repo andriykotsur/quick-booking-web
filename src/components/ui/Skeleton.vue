17716085c970884fcf6d312540230cceaf675d5f
Andriy Kotsur <kotsurandriy@gmail.com>
2025-06-01 17:47:07 +0200

    Develop Skeleton component

---
+++ src/components/ui/Skeleton.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { clsx } from 'clsx'
+import { type HTMLAttributes } from 'vue'
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+  variant: 'rectangle' | 'circle' | 'pill'
+}>()
+</script>
+
+<template>
+  <div
+    :class="
+      clsx([
+        'bg-neutral-700 animate-pulse',
+        {
+          rounded: props.variant === 'rectangle',
+          'rounded-xl': props.variant === 'pill',
+          'rounded-full': props.variant === 'circle',
+        },
+        props.class,
+      ])
+    "
+  ></div>
+</template>
