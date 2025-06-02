0abaccec0a68d42adfdad52230c863841087fea7
Andriy Kotsur <kotsurandriy@gmail.com>
2025-06-02 09:14:16 +0200

    Update icons list

--- src/icons/PanelCloseIcon.vue
+++ src/icons/PanelCloseIcon.vue
@@ -11,7 +11,7 @@
     stroke-linejoin="round"
   >
     <rect width="18" height="18" x="3" y="3" rx="2" />
-    <path d="M15 3v18" />
-    <path d="m10 15-3-3 3-3" />
+    <path d="M9 3v18" />
+    <path d="m16 15-3-3 3-3" />
   </svg>
 </template>
--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,3 +1,8 @@
 export { default as ArrowIcon } from './ArrowIcon.vue';
+export { default as BookIcon } from './BookIcon.vue';
 export { default as CloseIcon } from './CloseIcon.vue';
-export { default as PanelIcon } from './PanelIcon.vue';
+export { default as HouseIcon } from './HouseIcon.vue';
+export { default as PanelCloseIcon } from './PanelCloseIcon.vue';
+export { default as PanelOpenIcon } from './PanelOpenIcon.vue';
+export { default as SettingsIcon } from './SettingsIcon.vue';
+export { default as UtensilsIcon } from './UtensilsIcon.vue';
