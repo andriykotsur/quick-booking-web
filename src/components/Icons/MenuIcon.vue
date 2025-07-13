3f47f6eccdf8328bb4717d292274ff1597ecd338
Andriy Kotsur <kotsurandriy@gmail.com>
2025-07-13 15:27:38 +0200

    Move icons to components

--- src/components/Icons/MenuIcon.vue
+++ src/components/Icons/MenuIcon.vue
@@ -10,8 +10,8 @@
     stroke-linecap="round"
     stroke-linejoin="round"
   >
-    <rect width="18" height="18" x="3" y="3" rx="2" />
-    <path d="M9 3v18" />
-    <path d="m16 15-3-3 3-3" />
+    <path d="M4 12h16" />
+    <path d="M4 18h16" />
+    <path d="M4 6h16" />
   </svg>
 </template>
--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -2,7 +2,6 @@ export { default as ArrowIcon } from './ArrowIcon.vue';
 export { default as BookIcon } from './BookIcon.vue';
 export { default as CloseIcon } from './CloseIcon.vue';
 export { default as HouseIcon } from './HouseIcon.vue';
-export { default as PanelCloseIcon } from './PanelCloseIcon.vue';
-export { default as PanelOpenIcon } from './PanelOpenIcon.vue';
+export { default as MenuIcon } from './MenuIcon.vue';
 export { default as SettingsIcon } from './SettingsIcon.vue';
 export { default as UtensilsIcon } from './UtensilsIcon.vue';
